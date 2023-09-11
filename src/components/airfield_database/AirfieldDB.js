409960e2be089476bfee9666313bdf8ac7ec4ed8
Azhan <azhan@lawn-10-91-71-110.lawn.gatech.edu>
2023-09-11 13:34:07 -0400

    Added Dropdown for Airfield Switching and Airfield Info

--- src/components/airfield_database/AirfieldDB.js
+++ src/components/airfield_database/AirfieldDB.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AirfieldDB() {
-    return <div>Airfield Database</div>
+    const [selectedAirfield, setSelectedAirfield] = useState(null);
+
+    const airfields = [
+        { id: 'A', name: "Airfield A", info: "This is airfield A." },
+        { id: 'B', name: "Airfield B", info: "This is airfield B." },
+        { id: 'C', name: "Airfield C", info: "This is airfield C." },
+        { id: 'D', name: "Airfield D", info: "This is airfield D." },
+        { id: 'E', name: "Airfield E", info: "This is airfield E." },
+    ];
+
+    const handleDropdownChange = (e) => {
+        const selectedId = e.target.value;
+        const selected = airfields.find(airfield => airfield.id === selectedId);
+        setSelectedAirfield(selected);
+    };
+
+    return (
+        <div>
+            <h1>Airfield Database</h1>
+            <select defaultValue="" onChange={handleDropdownChange}>
+                <option value="" disabled>Select an Airfield</option>
+                {airfields.map((airfield, index) => (
+                    <option key={index} value={airfield.id}>
+                        {airfield.name}
+                    </option>
+                ))}
+            </select>
+
+            {selectedAirfield && (
+                <div>
+                    <p>You have selected: {selectedAirfield.name}</p>
+                    <p>Information: {selectedAirfield.info}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default AirfieldDB
+export default AirfieldDB;
