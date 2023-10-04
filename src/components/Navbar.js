327cea781f2241fd76e6f8ee416ef03462f8e64e
Azhan <azhan@lawn-10-91-51-146.lawn.gatech.edu>
2023-10-04 15:14:31 -0400

    Added a Centered logo on header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import logo from './Logo.png';
 
 function Navbar({ setActivePage }) {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Project Nightmare
-        </Typography>
-        <Button color="inherit" onClick={() => setActivePage('airfield_database')}>
-          Airfield Database
-        </Button>
-        <Button color="inherit" onClick={() => setActivePage('traffic_plot')}>
-          Traffic Plot
-        </Button>
-        <Button color="inherit" onClick={() => setActivePage('filedrop')}>
-          File Drop
-        </Button>
-        <Button color="inherit" onClick={() => setActivePage('route_planning')}>
-          Route Planning
-        </Button>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)', display: 'flex', alignItems: 'center' }}>
+          <img src={logo} alt="Project Nightmare Logo" style={{ marginRight: '0px', width: '275px', height: 'auto' }} />
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <Button color="inherit" onClick={() => setActivePage('airfield_database')}>
+            Airfield Database
+          </Button>
+          <Button color="inherit" onClick={() => setActivePage('traffic_plot')}>
+            Traffic Plot
+          </Button>
+          <Button color="inherit" onClick={() => setActivePage('filedrop')}>
+            File Drop
+          </Button>
+          <Button color="inherit" onClick={() => setActivePage('route_planning')}>
+            Route Planning
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
